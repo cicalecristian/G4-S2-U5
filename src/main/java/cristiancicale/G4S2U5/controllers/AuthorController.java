c52af546e6910357291ce5d56f62ec141d05ada8
cicalecristian <cicalecristian@gmail.com>
2026-04-16 17:10:13 +0200

    author controller completed

--- src/main/java/cristiancicale/G4S2U5/controllers/AuthorController.java
+++ src/main/java/cristiancicale/G4S2U5/controllers/AuthorController.java
@@ -1,11 +1,19 @@
 package cristiancicale.G4S2U5.controllers;
 
 import cristiancicale.G4S2U5.entities.Author;
+import cristiancicale.G4S2U5.exceptions.ValidationException;
+import cristiancicale.G4S2U5.payloads.AuthorDTO;
+import cristiancicale.G4S2U5.payloads.AuthorRespDTO;
 import cristiancicale.G4S2U5.payloads.NewAuthorPayload;
 import cristiancicale.G4S2U5.services.AuthorService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/authors")
@@ -19,36 +27,49 @@ public class AuthorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // 201
-    public Author saveAuthor(@RequestBody NewAuthorPayload body) {
+    public AuthorRespDTO saveUser(@RequestBody @Validated AuthorDTO body, BindingResult validationResult) {
 
-        return this.authorService.save(body);
+
+        if (validationResult.hasErrors()) {
+
+            List<String> errors = validationResult.getFieldErrors().stream().map(error -> error.getDefaultMessage()).toList();
+            throw new ValidationException(errors);
+        }
+
+        Author newAuthor = this.authorService.save(body);
+        return new AuthorRespDTO(newAuthor.getId());
     }
 
-    // 2. GET http://localhost:3001/users
     @GetMapping
     public Page<Author> getAuthors(@RequestParam(defaultValue = "0") int page,
                                    @RequestParam(defaultValue = "10") int size,
-                                   @RequestParam(defaultValue = "cognome") String sortBy) {
-
+                                   @RequestParam(defaultValue = "surname") String sortBy) {
         return this.authorService.findAll(page, size, sortBy);
     }
 
-    // 3. GET http://localhost:3001/users/{userId}
     @GetMapping("/{authorId}")
     public Author getById(@PathVariable long authorId) {
         return this.authorService.findById(authorId);
     }
 
-    // 4. PUT http://localhost:3001/users/{userId} (+ req.body)
     @PutMapping("/{authorId}")
     public Author getByIdAndUpdate(@PathVariable long authorId, @RequestBody NewAuthorPayload body) {
         return this.authorService.findByIdAndUpdate(authorId, body);
     }
 
-    // 5. DELETE http://localhost:3001/users/{userId}
     @DeleteMapping("/{authorId}")
     @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     public void getByIdAndDelete(@PathVariable long authorId) {
         this.authorService.findByIdAndDelete(authorId);
+    }
+
+    @PatchMapping("/{authorId}/avatar")
+    public void uploadAvatar(@RequestParam("profile_picture") MultipartFile file, @PathVariable long authorId) {
+
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+        System.out.println(file.getContentType());
+
+        this.authorService.avatarUpload(file, authorId);
     }
 }
